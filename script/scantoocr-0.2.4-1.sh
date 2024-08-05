3453183f66def5f42a275c3d0969e84d37a38198
Chris Wiggins <me@cwiggs.com>
2024-08-05 13:00:04 -0600

    feat: add error handling to scantoocr

--- script/scantoocr-0.2.4-1.sh
+++ script/scantoocr-0.2.4-1.sh
@@ -1,9 +1,10 @@
-#!/bin/bash
-# $1 = scanner device
-# $2 = friendly name
+#!/usr/bin/env bash
+set -eou pipefail
 
-#override environment, as brscan is screwing it up:
-export $(grep -v '^#' /opt/brother/scanner/env.txt | xargs)
+# Enabled for debugging
+# set +x
+
+# $1 = scanner device
 
 # Resolution (dpi):
 # 100,200,300,400,600
@@ -13,35 +14,60 @@ date=$(date +%Y-%m-%d-%H-%M-%S)
 filename_base=/scans/$date"-page"
 output_file=$filename_base"%d.pnm"
 
-if [ "`which usleep  2>/dev/null `" != '' ];then
-    usleep 100000
+# Import shell libraries
+source ./libs/error.sh
+
+# Check for required parameters
+if [ -z "${device}" ]; then
+  error "${LINENO}" "No device specified as arg1"
+fi
+
+# override environment, as brscan is screwing it up:
+if [ -f /opt/brother/scanner/env.txt ]; then
+  export $(grep -v '^#' /opt/brother/scanner/env.txt | xargs)
+fi
+
+if scanadf --device-name "$device" \
+  --source "Automatic Document Feeder(centrally aligned)" \
+  --resolution $resolution \
+  --output-file $output_file 2>/dev/null; then
+
+  echo "Scanning successful"
 else
-    sleep  0.1
-fi
-scanadf --device-name "$device" --source "Automatic Document Feeder(centrally aligned)" --resolution $resolution -o $output_file
-if [ ! -s $filename_base"1.pnm" ];then
-  if [ "`which usleep  2>/dev/null `" != '' ];then
-    usleep 1000000
-  else
-    sleep  1
-  fi
-  scanadf --device-name "$device" --source "Automatic Document Feeder(centrally aligned)" --resolution $resolution -o $output_file
+  error "${LINENO}" "Scanning failed"
 fi
 
-gm convert /scans/$date-page*.pnm /scans/$date.pdf
-rm /scans/$date-page*.pnm
+if gm convert "${filename_base}*.pnm" ${filename_base}.pdf ; then
+  echo "Conversion from pnm to pdf successful"
+else
+  error "${LINENO}" "Conversion from pnm to pdf failed"
+fi
 
-echo "Converting pdf to ocr pdf"
-output_ocr_pdf="${date}_ocr.pdf"
-ocrmypdf --rotate-pages \
+if [ -f "${filename_base}"*.pnm ]; then
+  echo "Removing temporary pnm files"
+  rm "${filename_base}"*.pnm
+fi
+
+output_ocr_pdf="${filename_base}_ocr.pdf"
+
+if ocrmypdf --rotate-pages \
   --clean \
   --quiet \
-  "/scans/${date}.pdf" \
-  "/scans/${output_ocr_pdf}"
+  "${filename_base}.pdf" \
+  "${output_ocr_pdf}" ; then
+  echo "OCR successful"
+else
+  error "${LINENO}" "OCR failed"
+fi
 
-rm /scans/"${date}".pdf
+rm "${filename_base}.pdf"
 
-/opt/brother/scanner/brscan-skey/script/trigger_inotify.sh "${SSH_USER}" "${SSH_PASSWORD}" "${SSH_HOST}" "${SSH_PATH}" "${output_ocr_pdf}"
+/opt/brother/scanner/brscan-skey/script/trigger_inotify.sh \
+  "${SSH_USER}" \
+  "${SSH_PASSWORD}" \
+  "${SSH_HOST}" \
+  "${SSH_PATH}" \
+  "${output_ocr_pdf}"
 
 /opt/brother/scanner/brscan-skey/script/sendtoftps.sh \
   "${FTP_USER}" \
